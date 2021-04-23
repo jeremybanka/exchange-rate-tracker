1050c19a3ec763d2d83ef9c0d7886aa38bf9e0aa
Jeremy Banka <hello@jeremybanka.com>
2021-04-23 15:33:28 -0700

    ✨ add an extensible single-page statemachine

---
+++ src/js/Log.js
@@ -0,0 +1,35 @@
+export default class Log {
+  constructor({ key = `data`, onRender }) {
+    this.key = key
+    this.onRender = onRender
+    this.load()
+    this.entries = this.entries || []
+    this.$render()
+  }
+
+  // BIZ
+  load() {
+    const foundSave = window.localStorage.getItem(`${this.key}-log`)
+    if (foundSave) this.entries = JSON.parse(foundSave)
+  }
+
+  save() {
+    window.localStorage.setItem(`${this.key}-log`, JSON.stringify(this.entries))
+  }
+
+  // UI
+  render() {
+    this.onRender()
+    this.save()
+  }
+
+  add(entry) {
+    this.entries = [entry, ...this.entries]
+    this.$render()
+  }
+
+  clear() {
+    this.entries = []
+    this.$render()
+  }
+}
