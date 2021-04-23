173917f879c10392f203ba55522a705146ad1075
Jeremy Banka <hello@jeremybanka.com>
2021-04-23 13:35:44 -0700

    ✨ add Market class with method that chains the aCurrency functionality after a fetch

--- src/js/core.js
+++ src/js/core.js
@@ -1,6 +1,10 @@
 import currencyDict from "./currency-dict.json"
 
-export default class Currency {
+/* eslint-disable prefer-destructuring */
+const API_KEY = process.env.API_KEY
+/* eslint-enable prefer-destructuring */
+
+class Currency {
   constructor(exchangeRateApiObj) {
     /* eslint-disable camelcase */
     const { base_code, conversion_rates } = exchangeRateApiObj
@@ -42,5 +46,21 @@ export default class Currency {
         name: nameAway,
       },
     }
+  }
+}
+
+export default class Market {
+  static convertCurrency({ from, to, howMany = 1 }) {
+    console.log(Currency.process)
+    console.log(Currency.convert)
+
+    return fetch(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${from}`)
+      .then(response => {
+        if (!response.ok) throw Error(response.statusText)
+        return response.json()
+      })
+      .then(response => Currency.process(response))
+      .then(response => Currency.convert({ original: response, to, howMany }))
+      .catch(error => error)
   }
 }
