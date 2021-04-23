55db081e8e55367a0af91664660be51f0bd93e28
Jeremy Banka <hello@jeremybanka.com>
2021-04-23 13:30:48 -0700

    ✨ add Currency class to parse api response w async static method to chain the constructor

--- src/js/core.js
+++ src/js/core.js
@@ -1,1 +1,15 @@
-export default number => number * 2
+export default class Currency {
+  constructor(exchangeRateApiObj) {
+    /* eslint-disable camelcase */
+    const { base_code, conversion_rates } = exchangeRateApiObj
+    this.name = Currency.nameFromCode(base_code)
+    this.code = base_code
+    this.rates = { ...conversion_rates }
+    /* eslint-enable camelcase */
+  }
+
+  static async process(exchangeRateApiObj) {
+    exchangeRateApiObj = new this(exchangeRateApiObj)
+    return exchangeRateApiObj
+  }
+}
