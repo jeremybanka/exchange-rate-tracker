14a53f6c9c5eee337aa6d936604b3a9a77cf4b2a
Jeremy Banka <hello@jeremybanka.com>
2021-04-23 16:37:11 -0700

    🚧 add tests that aren't working yet, need to research fetch and asynchrony

--- __test__/core.test.js
+++ __test__/core.test.js
@@ -1,9 +1,28 @@
-import double from "../src/js/core"
+import Market from "../src/js/core"
 
-describe(`double`, () => {
-  it(`doubles a number`, () => {
-    const input = 1
-    const expected = 2
-    expect(double(input)).toEqual(expected)
+const isValidAmountOfMoney = whatever => {
+  const numbersWithDotInMiddle = /\d*.\d*/i
+  const stringified = whatever.toString()
+  const conclusion = numbersWithDotInMiddle.test(stringified)
+  return conclusion
+}
+
+describe(`Market.convertCurrency(conversionRequest)`, () => {
+  it(`converts (1) USD to KRW (South Korean Won)`, async () => {
+    const conversionRequest = {
+      from: `USD`,
+      to: `KRW`,
+    }
+    return Market.convertCurrency(conversionRequest).then(response => {
+      const { home, away } = response
+      const assertion =
+        home.code === `USD` &&
+        away.code === `KPW` &&
+        home.name === `US Dollar` &&
+        away.name === `North Korean Won` &&
+        home.quantity === 1 &&
+        isValidAmountOfMoney(away.quantity) === true
+      expect(assertion).toBe(true)
+    })
   })
 })
