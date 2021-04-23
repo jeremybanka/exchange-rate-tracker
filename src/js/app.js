f4fcf42aff9d395468848e285e8888221d89b97d
Jeremy Banka <hello@jeremybanka.com>
2021-04-23 15:35:09 -0700

    ✨ wire some inputs and buttons to the fetch logic

--- src/js/app.js
+++ src/js/app.js
@@ -3,10 +3,48 @@ import "../styles/core.scss"
 import "../styles/font-face.scss"
 // UI
 import $ from "jquery"
+// CORE
+import Log from "./core"
 // BIZ
-import Market from "./core"
+import Market from "./Market"
+
+function $printEntries() {
+  const $makeEntry = entry => {
+    console.log(entry.home, entry.away)
+    console.log(entry.home, entry.away)
+    const $entry = $(`<section/>`)
+    const $home = $makeSide(entry.home, `home`)
+    const $away = $makeSide(entry.away, `away`)
+    return $entry.append($home, $away)
+  }
+  const $makeSide = (homeOrAway, which) => {
+    console.log(homeOrAway)
+    const { quantity, code } = homeOrAway
+    const $side = $(`<div/>`).addClass(which)
+    const $tale = $(`<div/>`).addClass(`tale`).text(quantity)
+    const $code = $(`<div/>`).addClass(`code`).text(code)
+    return $side.append($tale, $code)
+  }
+  const $entries = this.entries.map(entry => $makeEntry(entry))
+  $(`main`).empty()
+  $(`main`).append($entries)
+}
 
 $(() => {
-  console.log(process.env.API_KEY)
-  console.log(Market.convertCurrency({ from: `USD`, to: `KRW` }))
+  const log = new Log({ key: `conversion`, onRender: $printEntries })
+
+  $(`form`).on(`submit`, event => {
+    event.preventDefault()
+    console.log(`fired submit`)
+    const from = $(`input#from`).val()
+    const to = $(`input#to`).val()
+    const howMany = parseInt($(`input#how-many`).val(), 10)
+    Market.convertCurrency({ from, to, howMany }).then(conversion => {
+      log.add(conversion)
+    })
+    $(`input#how-many`).val(``)
+  })
+  $(`button#clear`).on(`click`, () => {
+    log.clear()
+  })
 })
