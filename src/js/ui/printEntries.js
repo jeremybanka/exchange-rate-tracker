0ff7b79bd268ce850f6a634b778f990207476703
Jeremy Banka <hello@jeremybanka.com>
2021-04-26 23:25:33 -0700

    ♻️  segment logic and add some gentle coaxing for pesky rounding errors

--- src/js/ui/printEntries.js
+++ src/js/ui/printEntries.js
@@ -1,23 +1,27 @@
 import $ from "jquery"
 
+const $makeEntry = entry => {
+  const $entry = $(`<section/>`)
+  const $home = $makeSide(entry.home, `home`)
+  const $away = $makeSide(entry.away, `away`)
+  if (entry.error) $entry.addClass(`error`)
+  return $entry.append($home, $away)
+}
+const $makeSide = (homeOrAway, which) => {
+  const { quantity, code } = homeOrAway
+  let tale
+  if (typeof quantity === `number` && quantity.toString().length > 6) {
+    tale = quantity.toPrecision(6)
+  }
+  const $side = $(`<div/>`).addClass(which)
+  const $tale = $(`<div/>`)
+    .addClass(`tale`)
+    .text(tale || quantity)
+  const $code = $(`<div/>`).addClass(`code`).text(code)
+  return $side.append($tale, $code)
+}
+
 export default function $printEntries() {
-  const $makeEntry = entry => {
-    console.log(entry.home, entry.away)
-    console.log(entry.home, entry.away)
-    const $entry = $(`<section/>`)
-    const $home = $makeSide(entry.home, `home`)
-    const $away = $makeSide(entry.away, `away`)
-    if (entry.error) $entry.addClass(`error`)
-    return $entry.append($home, $away)
-  }
-  const $makeSide = (homeOrAway, which) => {
-    console.log(homeOrAway)
-    const { quantity, code } = homeOrAway
-    const $side = $(`<div/>`).addClass(which)
-    const $tale = $(`<div/>`).addClass(`tale`).text(quantity)
-    const $code = $(`<div/>`).addClass(`code`).text(code)
-    return $side.append($tale, $code)
-  }
   const $entries = this.entries.map(entry => $makeEntry(entry))
   $(`main`).empty()
   $(`main`).append($entries)
